8a3275d0732d8248ae92a6e65ef80c6fabe7a06c
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 15:04:53 -0400

    Added box and storage classes

---
+++ csharp/src/JewelleryBox.cs
@@ -0,0 +1,22 @@
+﻿using NecklaceRefactoringKata.JewelleryTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecklaceRefactoringKata
+{
+    public class JewelleryBox
+    {
+        public readonly List<Ring> RingCompartment;
+        public readonly List<JewelleryBase> TopShelf;
+        public readonly List<JewelleryBase> MainSection;
+        public JewelleryBox()
+        {
+            RingCompartment = new List<Ring>();
+            TopShelf = new List<JewelleryBase>();
+            MainSection = new List<JewelleryBase>();
+        }
+    }
+}
---
+++ csharp/src/JewelleryStorage.cs
@@ -0,0 +1,31 @@
+﻿using NecklaceRefactoringKata.JewelleryTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecklaceRefactoringKata
+{
+    public class JewelleryStorage
+    {
+        public readonly JewelleryBox Box;
+        public readonly List<JewelleryBase> Tree;
+        public readonly List<JewelleryBase> TravelRoll;
+        public readonly List<JewelleryBase> Safe;
+        public readonly List<JewelleryBase> DresserTop;
+
+        public JewelleryStorage()
+        {
+            Box = new JewelleryBox();
+            Tree = new List<JewelleryBase>();
+            TravelRoll = new List<JewelleryBase>();
+            Safe = new List<JewelleryBase>();
+            DresserTop = new List<JewelleryBase>();
+        }
+        public bool IsInTravelRoll(JewelleryBase item)
+        {
+            return TravelRoll.Contains(item);
+        }
+    }
+}
