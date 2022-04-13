20a40924f4182380dd95503dd32ff587e557a8bd
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 15:34:51 -0400

    Cleanup usings

--- csharp/src/JewelleryStorage.cs
+++ csharp/src/JewelleryStorage.cs
@@ -1,9 +1,4 @@
 ﻿using NecklaceRefactoringKata.JewelleryTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NecklaceRefactoringKata
 {
