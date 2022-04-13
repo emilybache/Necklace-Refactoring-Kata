b1e46ed3cdcdc3da7f5d4e6408e574bf8614a661
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 14:14:24 -0400

    Added Jewellery base record type

---
+++ csharp/src/Models/Jewellery.cs
@@ -0,0 +1,18 @@
+﻿using NecklaceRefactoringKata.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecklaceRefactoringKata.Models
+{
+    public record Jewellery(Jewel Stone)
+    {
+        public virtual bool IsRing() { return false; }
+        public virtual bool IsSmall() { return false; }
+        public virtual bool IsEarring() { return false; }
+        public virtual bool IsNecklace() { return false; }
+        public virtual bool IsLarge() { return false; }
+    }
+}
