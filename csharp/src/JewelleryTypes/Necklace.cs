ac7cbb7f4d70203f0c19dd52054729db31f37dc8
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 14:52:03 -0400

    Add Necklace

--- csharp/src/JewelleryTypes/Necklace.cs
+++ csharp/src/JewelleryTypes/Necklace.cs
@@ -4,5 +4,13 @@ namespace NecklaceRefactoringKata.JewelleryTypes
 {
     public record Necklace(NecklaceType Type, Jewel Stone) : JewelleryBase(Stone)
     {
+        public override bool IsNecklace()
+        {
+            return true;
+        }
+        public override bool IsLarge()
+        {
+            return Type == NecklaceType.Beads || Type == NecklaceType.LongChain;
+        }
     }
 }
