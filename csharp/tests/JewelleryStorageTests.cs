a39dee86861e52b5febf29ca9cba964146e8b2d8
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 15:28:29 -0400

    Added one test for Pack() as an example

---
+++ csharp/tests/JewelleryStorageTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NecklaceRefactoringKata;
+using NecklaceRefactoringKata.Enums;
+using NecklaceRefactoringKata.JewelleryTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecklaceRefactoringKata.Tests
+{
+    [TestClass()]
+    public class JewelleryStorageTests
+    {
+        [TestMethod()]
+        public void PackTest_WhenEarringStud_PacksItInTopShelf()
+        {
+            //arrange
+            var jewelleryStorage = new JewelleryStorage();
+            var item = new Earring(Type: EarringType.Stud, Stone: Jewel.Amber);
+            
+            //act
+            Packer.Pack(item, jewelleryStorage);
+
+            //assert
+            CollectionAssert.Contains(jewelleryStorage.Box.TopShelf, item);
+        }
+
+        //To devs: Write more tests here before you refactor
+        //note: JewelleryBase types are records, so they automatically have value-based equality checks
+        //and can be used with CollectionAssert
+    }
+}
