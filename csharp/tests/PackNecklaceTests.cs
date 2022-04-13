7dc88a30761b3c596c7896bc4717bcefcd138e53
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 15:34:10 -0400

    Added one test against PackNecklace() as an example

---
+++ csharp/tests/PackNecklaceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NecklaceRefactoringKata;
+using NecklaceRefactoringKata.Enums;
+using NecklaceRefactoringKata.JewelleryTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecklaceRefactoringKata.Tests
+{
+    [TestClass()]
+    public class PackNecklaceTests
+    {
+        [TestMethod()]
+        public void Test_AmberBeadsNecklace_IsPackedInTree()
+        {
+            //arrange
+            var jewelleryStorage = new JewelleryStorage();
+            var item = new Necklace(Type: NecklaceType.Beads, Stone: Jewel.Amber);
+            
+            //act
+            Packer.PackNecklace(item, jewelleryStorage);
+
+            //assert
+            CollectionAssert.Contains(jewelleryStorage.Tree, item);
+        }
+
+        //To devs: Write more tests here before you refactor
+        
+        //note: JewelleryBase types are records, so they automatically have value-based equality checks
+        //and can be used with CollectionAssert
+    }
+}
