ece06e7b787107104a22948b0afc48e77605ce11
Maclain Wiltzer <mwiltzer@gmail.com>
2022-04-13 15:08:59 -0400

    Implement PackNecklace

--- csharp/src/Packer.cs
+++ csharp/src/Packer.cs
@@ -1,9 +1,27 @@
-﻿namespace NecklaceRefactoringKata
+﻿using NecklaceRefactoringKata.Enums;
+using NecklaceRefactoringKata.JewelleryTypes;
+
+namespace NecklaceRefactoringKata
 {
     public static class Packer
     {
-        //todo
-        public static void PackNecklace()
-        { }
+        public static void PackNecklace(Necklace item, JewelleryStorage storage)
+        {
+            if (item.Stone == Jewel.Diamond)
+                storage.Safe.Add(item);
+            else if (!item.IsLarge())
+                storage.Box.TopShelf.Add(item);
+            else if (item.Type == NecklaceType.Beads || item.Type == NecklaceType.Chain)
+                storage.Tree.Add(item);
+            else if (item is PendantNecklace pendantNecklace)
+            {
+                storage.Tree.Add(pendantNecklace.Chain);
+                storage.Box.TopShelf.Add(pendantNecklace.Pendant);
+            }
+        }
+        public static void Pack()
+        {
+
+        }
     }
 }
