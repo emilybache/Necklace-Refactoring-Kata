283e2a29dd76439c59bdaff5ebfb87fd112fd08f
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2022-04-14 11:29:49 +0200

    port Jewellery to PHP

---
+++ php/src/Jewellery.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace NecklaceRefactoringKata;
+
+enum Jewel {
+    case Plain;
+    case Diamond;
+    case Pearl;
+    case Amber;
+}
+
+class Jewellery
+{
+    public function __construct(private Jewel $jewel)
+    {
+    }
+
+    public function isRing(): bool
+    {
+        return false;
+    }
+
+    public function isSmall(): bool
+    {
+        return false;
+    }
+
+    public function isEarring(): bool
+    {
+        return false;
+    }
+
+    public function isNecklace(): bool
+    {
+        return false;
+    }
+
+    public function isLarge(): bool
+    {
+        return false;
+    }
+}
