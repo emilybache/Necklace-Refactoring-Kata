17d3c332af8723b8e510022563271930eb8420b4
Your Name <sfauvel@gmail.com>
2022-04-16 21:58:52 +0200

    Use doc as test

--- python/test_necklace_packing.py
+++ python/test_necklace_packing.py
@@ -3,6 +3,7 @@ from approvaltests import verify
 from jewellery_storage import *
 from packer import pack_necklace
 from fixtures import jewellery_storage, print_jewellery_storage
+from doc_as_test_pytest import DocAsTest, doc, doc_module
 
 
 def pack_necklace_item(item: Necklace, storage: JewelleryStorage) -> str:
@@ -13,37 +14,37 @@ def pack_necklace_item(item: Necklace, storage: JewelleryStorage) -> str:
     log += print_jewellery_storage(storage)
     return log
 
-def test_pack_pearl_necklace(jewellery_storage):
+def test_pack_pearl_necklace(doc, jewellery_storage):
     item = Necklace(stone=Jewel.Pearl, type=NecklaceType.Beads)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
 
-def test_pack_amber_necklace(jewellery_storage):
+def test_pack_amber_necklace(doc, jewellery_storage):
     item = Necklace(stone=Jewel.Amber, type=NecklaceType.Beads)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
 
-def test_pack_diamond_necklace(jewellery_storage):
+def test_pack_diamond_necklace(doc, jewellery_storage):
     item = Necklace(stone=Jewel.Diamond, type=NecklaceType.Chain)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
 
-def test_pack_chain(jewellery_storage):
+def test_pack_chain(doc, jewellery_storage):
     item = Necklace(stone=Jewel.Plain, type=NecklaceType.Chain)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
 
 
-def test_pack_pendant_necklace(jewellery_storage):
+def test_pack_pendant_necklace(doc, jewellery_storage):
     item = PendantNecklace(stone=Jewel.Amber,
                            chain=Necklace(stone=Jewel.Plain, type=NecklaceType.Chain),
                            pendant=Jewellery(stone=Jewel.Amber), type=NecklaceType.Pendant)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
 
-def test_pack_pendant_necklace_large_chain(jewellery_storage):
+def test_pack_pendant_necklace_large_chain(doc, jewellery_storage):
     item = PendantNecklace(stone=Jewel.Amber,
                            chain=Necklace(stone=Jewel.Plain, type=NecklaceType.LongChain),
                            pendant=Jewellery(stone=Jewel.Amber), type=NecklaceType.Pendant)
     log = pack_necklace_item(item, jewellery_storage)
-    verify(log)
+    doc.write(log)
