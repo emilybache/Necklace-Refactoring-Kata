b993f7c03ed3fd2999e29dcd6f51c569f35689c3
Your Name <sfauvel@gmail.com>
2022-04-16 23:24:38 +0200

    Display storage in a table

--- python/fixtures.py
+++ python/fixtures.py
@@ -11,13 +11,16 @@ def jewellery_storage():
 def print_jewellery_storage(storage: JewelleryStorage):
     contents = f"""\
 Jewellery Storage:
-    Box:
-        Ring Compartment: {storage.box.ring_compartment}
-        Top Shelf:        {storage.box.top_shelf}
-        Main Section:     {storage.box.main_section}
-    Tree:                 {storage.tree}
-    Travel Roll:          {storage.travel_roll}
-    Safe:                 {storage.safe}
-    On top of dresser:    {storage.dresser_top}
+
+[%autowidth]
+|====
+.3+| Box | Ring Compartment |   {storage.box.ring_compartment}
+| Top Shelf |                   {storage.box.top_shelf}
+| Main Section |                {storage.box.main_section}
+2+| Tree |                      {storage.tree}
+2+| Travel Roll |               {storage.travel_roll}
+2+| Safe |                      {storage.safe}
+2+| On top of dresser |         {storage.dresser_top}
+|====
 """
     return contents
