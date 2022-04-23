da33cde970f2dee35f0b100faaf41aad0cabf610
Ragunath Jawahar <ragunath@redgreen.io>
2022-04-23 13:02:43 +0530

    chore: add JUnit and Approvals dependencies

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -12,7 +12,12 @@ repositories {
 }
 
 dependencies {
-  testImplementation(kotlin("test"))
+  testImplementation(kotlin("test-junit5"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+
+  testImplementation("com.approvaltests:approvaltests:15.1.1")
 }
 
 tasks.test {
