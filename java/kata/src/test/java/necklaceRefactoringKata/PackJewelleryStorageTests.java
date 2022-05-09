c73bd2293ab3588c3009bdcbd6f1ba69078db505
Lars Eckart <lars.eckart@hey.com>
2022-05-09 11:50:21 +0300

    add test skeleton for new feature

--- java/kata/src/test/java/necklaceRefactoringKata/PackJewelleryStorageTests.java
+++ java/kata/src/test/java/necklaceRefactoringKata/PackJewelleryStorageTests.java
@@ -1,30 +1,35 @@
 package necklaceRefactoringKata;
 
-
+import necklaceRefactoringKata.jewellery.Earring;
+import necklaceRefactoringKata.jewellery.EarringType;
 import necklaceRefactoringKata.jewellery.Jewel;
 import necklaceRefactoringKata.jewellery.Necklace;
 import necklaceRefactoringKata.jewellery.NecklaceType;
 import necklaceRefactoringKata.jewellery.Pendant;
 import necklaceRefactoringKata.jewellery.PendantNecklace;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PackNecklaceTests {
+public class PackJewelleryStorageTests {
+
+    JewelleryStorage storage;
+
+    @BeforeEach
+    void setUp() {
+        storage = new JewelleryStorage();
+    }
 
     @Test
-    void test_pack_pearl_necklace() {
-        var item = new Necklace(Jewel.Pearl, NecklaceType.Beads);
-        var storage = new JewelleryStorage();
-        Packer.packNecklace(item, storage);
+    void test_pack_earring_stud() {
+        var item = new Earring(Jewel.Amber, EarringType.Stud);
+        Packer.pack(item, storage);
         // TODO: check it packed it correctly
     }
 
-    @Disabled("new feature")
     @Test
     void test_pack_diamond_pendant_necklace() {
         var item = new PendantNecklace(new Necklace(Jewel.Plain, NecklaceType.Chain), new Pendant(Jewel.Diamond));
-        var storage = new JewelleryStorage();
-        Packer.packNecklace(item, storage);
-        // TODO: check it packed it correctly
+        Packer.pack(item, storage);
+        // TODO: new feature - only the pendant should be in the safe, not the chain
     }
 }
