6c205fbe05f1799248685502177c20364ede70ae
emily <emily@bacheconsulting.com>
2024-05-08 15:23:33 +0200

    reset tests for exercise start in cpp - doctest and catch versions

--- cpp/test-catch2/sample_catch.cpp
+++ cpp/test-catch2/sample_catch.cpp
@@ -1,11 +1,39 @@
 #include "ApprovalTests.hpp"
 #include "catch2/catch.hpp"
 
+#include "Necklace.h"
+#include "JewelleryStorage.h"
+#include "Pendant.h"
+#include "Packer.h"
+#include "Ring.h"
 
-TEST_CASE ("Sample") {
-    SECTION("sample section") {
-        REQUIRE(true == false);
+TEST_CASE("Pack") {
+    SECTION("PackEarringStud") {
+        // arrange
+        auto item = new Earring(EarringType::Stud, Jewel::Amber);
+        auto storage = new JewelleryStorage("Storage");
+
+        // act
+        Packer::Pack(item, storage);
+
+        // assert
+        auto v = storage->Box.TopShelf;
+
+        REQUIRE(std::find(v.begin(), v.end(), item) != v.end());
+    }
+
+    SECTION("Pack_Diamond_Pendant_Necklace") {
+        // arrange
+        auto item = new PendantNecklace(new Necklace(NecklaceType::Chain, Jewel::Plain),
+                                        new class Pendant(Jewel::Diamond));
+        auto storage = new JewelleryStorage("Storage");
+
+        // act
+        Packer::Pack(item, storage);
+
+        // assert
+        // TODO: pendant should be in safe but not chain
+
     }
 }
-
 
--- cpp/test-doctest/sample_doctest.cpp
+++ cpp/test-doctest/sample_doctest.cpp
@@ -1,11 +1,39 @@
 #include "ApprovalTests.hpp"
 #include "doctest/doctest.h"
 
+#include "Necklace.h"
+#include "JewelleryStorage.h"
+#include "Pendant.h"
+#include "Packer.h"
+#include "Ring.h"
 
-TEST_CASE ("Sample") {
-    SUBCASE("sample section") {
-        REQUIRE(true == false);
+
+TEST_CASE("Pack") {
+    SUBCASE("PackEarringStud") {
+        // arrange
+        auto item = new Earring(EarringType::Stud, Jewel::Amber);
+        auto storage = new JewelleryStorage("Storage");
+
+        // act
+        Packer::Pack(item, storage);
+
+        // assert
+        auto v = storage->Box.TopShelf;
+
+        REQUIRE(std::find(v.begin(), v.end(), item) != v.end());
+    }
+
+    SUBCASE("Pack_Diamond_Pendant_Necklace") {
+        // arrange
+        auto item = new PendantNecklace(new Necklace(NecklaceType::Chain, Jewel::Plain),
+                                        new class Pendant(Jewel::Diamond));
+        auto storage = new JewelleryStorage("Storage");
+
+        // act
+        Packer::Pack(item, storage);
+
+        // assert
+        // TODO: pendant should be in safe but not chain
+
     }
 }
-
-
