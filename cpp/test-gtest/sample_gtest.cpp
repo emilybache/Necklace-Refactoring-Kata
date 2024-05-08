2efc0a08d7b05e3899dd906260386d58bbb96b0b
emily <emily@bacheconsulting.com>
2024-05-08 14:12:03 +0200

    reset tests for exercise start in cpp

--- cpp/test-gtest/sample_gtest.cpp
+++ cpp/test-gtest/sample_gtest.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "ApprovalTests.hpp"
 #include "Necklace.h"
 #include "JewelleryStorage.h"
@@ -8,156 +9,29 @@
 
 using namespace std;
 
-JewelleryStorage* packNecklaceItem(Necklace* item) {
+TEST(PackTests, PackEarringStud) {
+    // arrange
+    auto item = new Earring(EarringType::Stud, Jewel::Amber);
     auto storage = new JewelleryStorage("Storage");
-    Packer::PackNecklace(item, storage);
-    return storage;
-}
-JewelleryStorage* packItem(JewelleryBase* item, bool travelRoll = false) {
-    auto storage = new JewelleryStorage("Storage");
-    if (travelRoll) {
-        storage->TravelRoll.emplace_back(item);
-    }
+    
+    // act
     Packer::Pack(item, storage);
-    return storage;
-}
-
-TEST(PackNecklaceTests, PackPearlNecklace) {
-    auto item = new Necklace(NecklaceType::Beads, Jewel::Pearl);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackAmberNecklace)
-{
-    auto item = new Necklace(NecklaceType::Beads, Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackAmberChainNecklace)
-{
-    auto item = new Necklace(NecklaceType::Chain, Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackDiamondNecklace)
-{
-    auto item = new Necklace(NecklaceType::Chain, Jewel::Diamond);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackChain)
-{
-    auto item = new Necklace(NecklaceType::Chain, Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackLongChain)
-{
-    auto item = new Necklace(NecklaceType::LongChain, Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-
-TEST(PackNecklaceTests, PackPendantNecklace)
-{
-    auto item = new PendantNecklace(
-            new Necklace(NecklaceType::Chain, Jewel::Plain),
-            new class Pendant(Jewel::Amber));
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackNecklaceTests, PackPendantNecklaceLongChain)
-{
-    auto item = new PendantNecklace(
-            new Necklace(NecklaceType::LongChain, Jewel::Plain),
-            new class Pendant(Jewel::Amber));
-    ApprovalTests::Approvals::verify(*packNecklaceItem(item));
-}
-
-TEST(PackTests, Pack_Earring_Stud)
-{
-    auto item = new Earring(EarringType::Stud,  Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Diamond_Earring_Stud)
-{
-    auto item = new Earring(EarringType::Stud,  Jewel::Diamond);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Earring_Hoop)
-{
-    auto item = new Earring(EarringType::Hoop,  Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Earring_Drop)
-{
-    auto item = new Earring(EarringType::Drop,  Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Earring_Drop_with_Stone)
-{
-    auto item = new Earring(EarringType::Drop,  Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Earring_From_Travel_roll)
-{
-    auto item = new Earring(EarringType::Drop,  Jewel::Plain);
-    bool travelRoll = true;
-    ApprovalTests::Approvals::verify(*packItem(item, travelRoll));
-}
-
-TEST(PackTests, Pack_Necklace_From_Travel_roll)
-{
-    auto item = new Necklace(NecklaceType::Beads,  Jewel::Pearl);
-    bool travelRoll = true;
-    ApprovalTests::Approvals::verify(*packItem(item, travelRoll));
-}
-
-TEST(PackTests, Pack_Amber_Necklace)
-{
-    auto item = new Necklace(NecklaceType::Beads,  Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, PackAmberChainNecklace)
-{
-    auto item = new Necklace(NecklaceType::Chain,  Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Long_Chain_Necklace)
-{
-    auto item = new Necklace(NecklaceType::LongChain,  Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Pendant_Necklace)
-{
-    auto item = new PendantNecklace(new Necklace(NecklaceType::Chain, Jewel::Plain),
-                                   new class Pendant(Jewel::Amber));
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
-
-TEST(PackTests, Pack_Pendant)
-{
-    auto item = new class Pendant(Jewel::Amber);
-    ApprovalTests::Approvals::verify(*packItem(item));
+    
+    // assert
+    ASSERT_THAT(storage->Box.TopShelf, testing::Contains(item));
 }
 
 TEST(PackTests, Pack_Diamond_Pendant_Necklace)
 {
-    auto item = new PendantNecklace(new Necklace(NecklaceType::Chain, Jewel::Plain),
-                                   new class Pendant(Jewel::Diamond));
-    ApprovalTests::Approvals::verify(*packItem(item));
-}
+    // arrange
+    auto item = new PendantNecklace(new Necklace(NecklaceType::Chain, Jewel::Plain), 
+                                    new class Pendant(Jewel::Diamond));
+    auto storage = new JewelleryStorage("Storage");
 
-TEST(PackTests, Pack_UnknownItem)
-{
-    auto item = new Ring(Jewel::Plain);
-    ApprovalTests::Approvals::verify(*packItem(item));
+    // act
+    Packer::Pack(item, storage);
+    
+    // assert
+    // TODO: pendant should be in safe but not chain
+    
 }
